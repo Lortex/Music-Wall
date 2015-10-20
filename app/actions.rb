994517420bbec9a35148fec133cac4283aca7435
Wilson Chu <wilsonjarrodchu@hotmail.com>
2015-10-20 00:52:42 +0000

    Adding validation errors and cleaning code up

--- app/actions.rb
+++ app/actions.rb
@@ -3,7 +3,25 @@ get '/' do
   erb :index
 end
 
-get '/soundtrack' do
-  @soundtrack = Song.all
+get '/songs' do
+  @songs = Song.all
   erb :'songs/index'
 end
+
+get '/songs/new' do
+  erb :'songs/new'
+end
+
+post '/songs' do
+  @song = Song.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url]
+  )
+  if @song.save
+    redirect '/songs'
+  else
+    erb :'songs/new'
+  end
+end
+    
